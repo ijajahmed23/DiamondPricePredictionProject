9c072184030b55813a78e5fcf3b5d97b3163fe03
ijajahmed23 <ijajahmed1997@gmail.com>
2023-05-15 22:43:24 +0530

    refresh

---
+++ src/utils.py
@@ -0,0 +1,54 @@
+import sys
+import os
+import pickle
+import numpy as np
+import pandas as pd
+
+
+from src.exception import CustomException
+from src.logger import logging
+from sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
+
+def save_object(file_path,obj):
+
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+
+        with open(file_path,'wb') as file_obj:
+            pickle.dump(obj,file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+        for i in range(len(models)):
+            model=list(models.values())[i]
+            # Train model
+            model.fit(X_train,y_train)
+
+
+            # Predict Testing data
+            y_test_pred=model.predict(X_test)
+
+            #Get r2 scores for train and test data
+            test_model_score=r2_score(y_test,y_test_pred)
+            report[list(models.values())[i]]=test_model_score
+
+        return report
+    
+    except Exception as e:
+        logging.info('exceptions occurred during model training')
+        raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+        
+    except Exception as e:
+        logging.info('Exception occurred in load_object function utils')
+        raise CustomException(e,sys)
